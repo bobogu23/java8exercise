daab970a17a6c8aee493c26b1111edae86c0f635
bobogu23 <bsg_24@126.com>
2017-06-24 20:56:41 +0800

    java8exercise

---
+++ src/test/java/com/gu/exercise/LambdaTest.java
@@ -0,0 +1,40 @@
+package com.gu.exercise;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class LambdaTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public LambdaTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( LambdaTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+
+}
